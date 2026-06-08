062cbc79a39c23072857b4ded7fdfad05a8aabd4
rajeshvadla7799 <rajesh.vadla7799@gmail.com>
2026-06-08 22:07:36 +0530

    commit for shell

---
+++ 12-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/Shell_scripts"
+LOGS_FILE="/var/log/Shell_scripts/$0.log"
+echo "USERID=$USERID"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script as root user"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+    if [ $? -ne 0 ]; then
+        echo "$2... Failure"
+    exit 1
+    else
+        echo "$2... Success"
+    fi
+}
+
+apt install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+apt install mysql-server -y &>> $LOGS_FILE    
+VALIDATE $? "Installing MySQL"
+
+apt install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing Node.js"
