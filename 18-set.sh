ea46b654fd5002f92923b4782f85bb84fabe2cde
rajeshvadla7799 <rajesh.vadla7799@gmail.com>
2026-06-09 11:30:59 +0530

    commit for shell

---
+++ 18-set.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/Shell_scripts"
+LOGS_FILE="/var/log/Shell_scripts/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+N="\e[0m"
+
+set -e # this will be checking for errors, if errors it will exit
+
+if [ $USERID -ne 0 ]; then
+    echo "$R Please run this script as root user $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+    if [ $? -ne 0 ]; then
+        echo -e "$R $2... Failure $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$G $2... Success $N" | tee -a $LOGS_FILE
+    fi
+}
+
+for pachage in $@ # sudo sh 14-loops.sh nginx mysql-server nodejs
+do
+    apt list installed $pachage &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo -e "$Y $pachage is not installed, installing Now $N"
+        apt install $pachage -y &>>$LOGS_FILE
+        #VALIDATE $? "$pachage installation"
+    else
+        echo -e " $pachage is already installed, $G skipping installation $N"
+    fi
+done
